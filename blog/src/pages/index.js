ccd5c2ba32bbb2a5fb5e1b9dfd07d6458b773cb9
Khaner-ejazkhan <54853472+Khaner-ejazkhan@users.noreply.github.com>
2021-03-03 20:21:09 -0600

    Initialize activity-7

---
+++ blog/src/pages/index.js
@@ -0,0 +1,51 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import { Layout } from "../components/layout"
+import { SEO } from "../components/seo"
+import { List, ListItem } from "../components/List"
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <List width={[1, 1 / 2, 2 / 3]} p={2}>
+    {
+      data.allContentfulBlogPost.edges.map(edge => (
+        <ListItem p={3}>
+          <Link to={edge.node.slug} key={edge.node.id}>{edge.node.title}</Link>
+          <div>
+            <img src={edge.node.heroImage.fluid.src} alt="hero image" />
+          </div>
+          <div>{edge.node.body.childMarkdownRemark.excerpt}</div>
+        </ListItem>
+      ))
+    }
+    </List>
+  </Layout>
+)
+
+export default IndexPage
+
+export const query = graphql`
+{
+  allContentfulBlogPost {
+    edges {
+      node {
+        id
+        title
+        slug
+        heroImage {
+          fluid(maxWidth: 600) {
+            src
+          }
+        }
+        body {
+          childMarkdownRemark {
+            excerpt
+          }
+        }
+      }
+    }
+  }
+}
+`
