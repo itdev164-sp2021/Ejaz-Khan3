671fb409077332e3eaafbf53bca768e2dddf59e3
Khaner-ejazkhan <54853472+Khaner-ejazkhan@users.noreply.github.com>
2021-03-03 21:26:11 -0600

    Update gatsby-config.js

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -4,7 +4,7 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Cool Blog`,
+    title: `Ejaz Khans Student MATC Blog`,
     description: `ITDEV-164 Gatsby Blog.`,
     author: `Mike Hunsicker`,
     contact: {
