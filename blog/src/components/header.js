57264e8ad72d1473397128b7ffb68097a873a11f
Khaner-ejazkhan <54853472+Khaner-ejazkhan@users.noreply.github.com>
2021-03-03 20:21:08 -0600

    Initialize activity-4

---
+++ blog/src/components/header.js
@@ -0,0 +1,46 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import styled from 'styled-components'
+
+const Outer = styled.header`
+  background: ${props => props.theme.header.backgroundColor};
+  margin-bottom: 1.45rem
+`
+
+const Inner = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const H1 = styled.h1`
+  margin: 0
+`
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
+
+const Header = ({ siteTitle }) => (
+  <Outer>
+    <Inner>
+      <H1>
+        <StyledLink to="/">
+          {siteTitle}
+        </StyledLink>
+      </H1>
+    </Inner>
+  </Outer>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
