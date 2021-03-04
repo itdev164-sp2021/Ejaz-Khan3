cfda0d1b8c40b73de378855f368d8618f5646f96
Khaner-ejazkhan <54853472+Khaner-ejazkhan@users.noreply.github.com>
2021-03-03 20:21:07 -0600

    Initialize activity-2

---
+++ blog/src/pages/index.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <ul>
+    {
+      data.allContentfulBlogPost.edges.map(edge => (
+        <li>
+          <Link to={edge.node.slug} key={edge.node.id}>{edge.node.title}</Link>
+        </li>
+      ))
+    }
+    </ul>
+  </Layout>
+)
+
+export default IndexPage
+
+export const query = graphql`
+  {
+    allContentfulBlogPost {
+      edges {
+        node {
+          id
+          title
+          slug
+        }
+      }
+    }
+  }
+`
